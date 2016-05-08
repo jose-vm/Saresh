d64ecfb72dda29f817ce72179da684b1470cb115
Jose Vazquez <cabo.rambo@hotmail.com>
2016-05-07 23:54:01 -0500

    Bump MiNET version + new variables

--- Saresh/NbtTags.cs
+++ Saresh/NbtTags.cs
@@ -2,6 +2,35 @@
 {
     public class NbtTags
     {
-        public static string TAG_ENTITYTYPEID = "id";
+        public static readonly string TAG_ENTITY_TYPEID = "id";
+        public static readonly string TAG_ENTITY_POS = "Pos";
+        public static readonly string TAG_ENTITY_MOTION = "Motion";
+        public static readonly string TAG_ENTITY_ROTATION = "Rotation";
+        public static readonly string TAG_ENTITY_FALLDISTANCE = "FallDistance";
+        public static readonly string TAG_ENTITY_FIRE = "Fire";
+        public static readonly string TAG_ENTITY_AIR = "Air";
+        public static readonly string TAG_ENTITY_ONGROUND = "OnGround";
+        public static readonly string TAG_ENTITY_DIMENSION = "Dimension";
+        public static readonly string TAG_ENTITY_INVULNERABLE = "Invulnerable";
+        public static readonly string TAG_ENTITY_PORTALCOOLDOWN = "PortalCooldown";
+        public static readonly string TAG_ENTITY_UUIDMOST = "UUIDMost";
+        public static readonly string TAG_ENTITY_UUIDLEAST = "UUIDLeast";
+        public static readonly string TAG_ENTITY_UUID = "UUID";
+        public static readonly string TAG_ENTITY_CUSTOMNAME = "CustomName";
+        public static readonly string TAG_ENTITY_CUSTOMNAMEVISIBLE = "CustomNameVisible";
+        public static readonly string TAG_ENTITY_SILENT = "Silent";
+        public static readonly string TAG_ENTITY_RIDING = "Riding";
+        public static readonly string TAG_ENTITY_PASSENGERS = "Passengers";
+        public static readonly string TAG_ENTITY_GLOWING = "Glowing";
+        public static readonly string TAG_ENTITY_TAGS = "Tags";
+
+        public static readonly string TAG_SCHEM_WIDTH = "Width";
+        public static readonly string TAG_SCHEM_HEIGHT = "Height";
+        public static readonly string TAG_SCHEM_LENGTH = "Length";
+        public static readonly string TAG_SCHEM_MATERIALS = "Materials";
+        public static readonly string TAG_SCHEM_BLOCKS = "Blocks";
+        public static readonly string TAG_SCHEM_DATA = "Data";
+        public static readonly string TAG_SCHEM_ENTITIES = "Entities";
+        public static readonly string TAG_SCHEM_TILEENTITIES = "TileEntities";
     }
 }
